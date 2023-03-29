848a576a9cd0bec5b7c4d037d51d5ca34b008201
arielZarate <arieltecnico@gmail.com>
2023-03-29 19:28:51 -0300

    primer commit del deploy a railway

---
+++ page/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Prueba de deployar en gitHub pages</h1>
+        <p>esta app sirve de prueba para probar los hosting de front </p>
+        <p>vercel</p>
+        <p>gitHub Pages</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
